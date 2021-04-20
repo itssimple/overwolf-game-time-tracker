e35c284556c2b8c88e992e4264ae9db052f4a551
NoLifeKing <webmaster@itssimple.se>
2021-04-20 20:41:43 +0200

    feat: Fixed detection of Teamfight Tactics

--- resources/scripts/background.js
+++ resources/scripts/background.js
@@ -19,6 +19,10 @@ function openWindow(event) {
 
 function gameLaunched(game) {
   if (game) {
+    if (window.possibleGameName && window.possibleGameName != null) {
+      game.title = window.possibleGameName;
+    }
+
     eventEmitter.emit("game-launched", game);
   }
 }
@@ -32,6 +36,65 @@ function gameInfoUpdated(game) {
   ) {
     eventEmitter.emit("game-exited", game);
   }
+}
+
+/**
+ * Will contain a possible game name replacement,
+ */
+window.possibleGameName = null;
+
+function launcherInfoUpdates(info) {
+  if (info && info.launcherClassId && info.launcherClassId == 10902) {
+    if (
+      info.feature &&
+      info.feature == "lobby_info" &&
+      info.info &&
+      info.info.lobby_info &&
+      info.info.lobby_info.queueId
+    ) {
+      switch (info.info.lobby_info.queueId) {
+        case "1090":
+        case "1100":
+        case "1130":
+          window.possibleGameName = "Teamfight Tactics";
+          break;
+        default:
+          window.possibleGameName = null;
+          break;
+      }
+    }
+  }
+}
+
+function setLauncherEvents(finishedSettingFeatures) {
+  overwolf.games.launchers.events.setRequiredFeatures(
+    10902,
+    ["lobby_info"],
+    function (info) {
+      if (info.status == "error") {
+        setTimeout(function () {
+          setLauncherEvents(finishedSettingFeatures);
+        }, 2000);
+        return;
+      }
+      log("[LOL-LAUNCHER]", info);
+      if (finishedSettingFeatures) {
+        finishedSettingFeatures(info);
+      }
+    }
+  );
+}
+
+function onLauncherLaunched(launcherInfo) {
+  log("[LAUNCHER]", launcherInfo);
+  if (launcherInfo.classId == 10902) {
+    setLauncherEvents();
+  }
+}
+
+function onLauncherTerminated(result) {
+  log("[LAUNCHER]", result);
+  window.possibleGameName = null;
 }
 
 if (firstLaunch) {
@@ -59,6 +122,41 @@ if (firstLaunch) {
 
   overwolf.games.onGameInfoUpdated.removeListener(gameInfoUpdated);
   overwolf.games.onGameInfoUpdated.addListener(gameInfoUpdated);
+
+  overwolf.games.launchers.events.onInfoUpdates.removeListener(
+    launcherInfoUpdates
+  );
+  overwolf.games.launchers.events.onInfoUpdates.addListener(
+    launcherInfoUpdates
+  );
+
+  overwolf.games.launchers.onLaunched.removeListener(onLauncherLaunched);
+  overwolf.games.launchers.onLaunched.addListener(onLauncherLaunched);
+
+  overwolf.games.launchers.onTerminated.removeListener(onLauncherTerminated);
+  overwolf.games.launchers.onTerminated.addListener(onLauncherTerminated);
+
+  overwolf.games.launchers.getRunningLaunchersInfo(function (res) {
+    if (res && res.success && res.launchers.length > 0) {
+      for (let launcher of res.launchers) {
+        if (launcher.classId == 10902) {
+          setLauncherEvents(function (info) {
+            overwolf.games.launchers.events.getInfo(10902, function (data) {
+              launcherInfoUpdates({
+                launcherClassId: res.launchers[0].classId,
+                feature: "lobby_info",
+                info: {
+                  lobby_info: {
+                    queueId: data.res.lobby_info.queueId,
+                  },
+                },
+              });
+            });
+          });
+        }
+      }
+    }
+  });
 
   window.eventEmitter.addEventListener("game-launched", function (gameInfo) {
     db.newGameSession(gameInfo);
