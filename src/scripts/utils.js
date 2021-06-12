bb5109085657efad1dfdb52b05b079c8b71c8c2c
NoLifeKing <webmaster@itssimple.se>
2021-06-12 14:17:07 +0200

    feat: Added generateUUID to give detected sessions a unique sessionId

--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -135,3 +135,26 @@ function getTimeObject(differenceInSeconds) {
 function pluralize(count, singular, plural) {
   return `${count} ${count === 1 ? singular : plural}`;
 }
+
+/**
+ * Method copied from Stackoverflow (https://stackoverflow.com/a/8809472/1025823)
+ * @returns {string} Returns a UUIDv4
+ */
+function generateUUID() {
+  // Public Domain/MIT
+  var d = new Date().getTime(); //Timestamp
+  var d2 = (performance && performance.now && performance.now() * 1000) || 0; //Time in microseconds since page-load or 0 if unsupported
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+    var r = Math.random() * 16; //random number between 0 and 16
+    if (d > 0) {
+      //Use timestamp until depleted
+      r = (d + r) % 16 | 0;
+      d = Math.floor(d / 16);
+    } else {
+      //Use microseconds since page-load if supported
+      r = (d2 + r) % 16 | 0;
+      d2 = Math.floor(d2 / 16);
+    }
+    return (c === "x" ? r : (r & 0x3) | 0x8).toString(16);
+  });
+}
