9dbdac6bd0b9c143d148da66aef4fcd19d805dc6
NoLifeKing <webmaster@itssimple.se>
2021-06-23 12:39:37 +0200

    chore: Updated package-packager to not include the src-folder

--- CreateNewPackage.ps1
+++ CreateNewPackage.ps1
@@ -1,5 +1,5 @@
 Remove-Item .\game-time-tracker.opk, .\game-time-tracker.zip -ErrorAction Ignore
 Write-Output "Compressing new version into archive, please hold on"
 #Compress-Archive -Path .\manifest.json,.\resources,.\windows -DestinationPath .\game-time-tracker.zip
-7z a -tzip .\game-time-tracker.opk -r @package-source.lst -xr!node_modules
+7z a -tzip .\game-time-tracker.opk -r @package-source.lst -xr!node_modules -xr!src
 Write-Output "New OPK generated"
